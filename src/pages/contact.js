993a4acb3a1434fc0b9db330c43d058389d6cfac
MarkDurksen <markdurksen@gmail.com>
2019-11-29 14:43:47 -0500

    Update form email type to 'email'

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,14 +5,14 @@ export default () => (
     <Layout>
         <h1>Bookings and Inquiries</h1>
         <form 
-            name="contact-form"
+            name="contact"
             method="post"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
         >
             <input name="name" placeholder="Your Name" type="text"/>
-            <input name="email" placeholder="email@example.com" type="text"/>
-            <textarea name="message" id="" cols="30" rows="10" />
+            <input name="email" placeholder="name@example.com" type="email"/>
+            <textarea name="message" />
             <button>Send</button>
         </form>
     </Layout>
