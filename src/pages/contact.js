3db2038d8b587d24b79a2ab20794a804ae87587a
MarkDurksen <markdurksen@gmail.com>
2019-11-29 14:18:18 -0500

    Basic contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,9 +3,17 @@ import Layout from "../components/layout"
 
 export default () => (
     <Layout>
-        <h1>For bookings and inquiries:</h1>
-        <p>
-            <a href="mailto:joncorbinmusic@gmail.com">joncorbinmusic@gmail.com</a>
-        </p>
+        <h1>Bookings and Inquiries</h1>
+        <form 
+            name="contact-form"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+        >
+            <input name="name" placeholder="Your Name" type="text"/>
+            <input name="email" placeholder="email@example.com" type="text"/>
+            <textarea name="message" id="" cols="30" rows="10" />
+            <button>Send</button>
+        </form>
     </Layout>
 )
