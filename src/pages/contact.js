182c08f1f62af902ed62986188c0008639c8e16d
MarkDurksen <markdurksen@gmail.com>
2019-12-02 18:19:33 -0500

    Style contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,25 +5,34 @@ export default () => (
     <Layout>
         <h1>Contact</h1>
         <p>For bookings and inquires please fill out the form below.</p>
-        <form 
+        <form
             name="contact"
             method="post"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
         >
             <input type="hidden" name="form-name" value="contact" />
-            <label>
-                Name
-                <input type="text" name="name" id="name" />
-            </label>
-            <label>
-                Email
-                <input type="email" name="email" id="email" />
-            </label>
-            <label>
-                Message
-                <textarea name="message" id="message" rows="5" />
-            </label>
+                <input
+                    className="form-input"
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Name"
+                />
+                <input
+                    className="form-input"
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Your email address"
+                />
+                <textarea
+                    className="form-input form-textarea"
+                    name="message"
+                    id="message"
+                    rows="5"
+                    placeholder="Message"
+                />
             <input type="hidden" name="bot-field"/>
             <button type="submit">Send</button>
             <input type="reset" value="Clear" />
